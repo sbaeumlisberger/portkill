58aaa874c5226c97bede2b7936cf5e8909e092d5
sbaeumlisberger <s.baeumlisberger@live.de>
2025-09-28 14:29:42 +0200

    Add app icon and create start menu entry on start

--- PortKill/MainWindow.xaml.cs
+++ PortKill/MainWindow.xaml.cs
@@ -1,8 +1,14 @@
 using Microsoft.UI.Xaml;
 using System;
-using WinUIEx;
 using System.Diagnostics;
 using System.IO;
+using Windows.Win32;
+using Windows.Win32.UI.Shell;
+using Windows.Win32.System.Com;
+using WinUIEx;
+using Windows.UI.Popups;
+using System.Threading.Tasks;
+using Microsoft.UI;
 
 namespace PortKill;
 
@@ -14,7 +20,44 @@ public sealed partial class MainWindow : Window
     {
         this.SetWindowSize(400, 200);
         ExtendsContentIntoTitleBar = true;
+        AppWindow.TitleBar.ButtonForegroundColor = App.Current.RequestedTheme == ApplicationTheme.Light ? Colors.Black : Colors.White;
+        AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets", "icon.ico"));
+
         InitializeComponent();
+
+        CreateStartMenuEntry();
+    }
+
+    private unsafe void CreateStartMenuEntry()
+    {
+        try
+        {
+            var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu), "Programs", "PortKill.lnk");
+            if (!File.Exists(filePath))
+            {
+                var exePath = Environment.ProcessPath!;
+
+                Guid CLSID_ShellLink = new("00021401-0000-0000-C000-000000000046");
+                PInvoke.CoCreateInstance(CLSID_ShellLink, null, CLSCTX.CLSCTX_INPROC_SERVER, out IShellLinkW* shellLink).ThrowOnFailure();
+
+                shellLink->SetPath(exePath);
+                shellLink->SetDescription("PortKill");
+                shellLink->SetWorkingDirectory(Path.GetDirectoryName(exePath)!);
+                shellLink->SetIconLocation(exePath, 0);
+
+                Guid IID_IPersistFile = new("0000010B-0000-0000-C000-000000000046");
+                shellLink->QueryInterface(IID_IPersistFile, out void* persistFilePtr).ThrowOnFailure();
+                var persistFile = (IPersistFile*)persistFilePtr;
+                persistFile->Save(filePath, true);
+
+                persistFile->Release();
+                shellLink->Release();
+            }
+        }
+        catch (Exception e)
+        {
+            Trace.WriteLine($"Could not create Start Menu entry: {e}");
+        }
     }
 
     private void StackPanel_Loaded(object sender, RoutedEventArgs e)
